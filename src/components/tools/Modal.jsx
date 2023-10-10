0a3a8b594589ab95f794989bf22d5045f54b81b0
Hanksito <barjabalseiro@gmail.com>
2023-10-10 23:14:55 +0200

    alert puesto

--- src/components/tools/Modal.jsx
+++ src/components/tools/Modal.jsx
@@ -124,36 +124,7 @@ const Modal = ({ setOpen, producto }) => {
   fetch("https://regalocompra.com/api/v1/registro", options)
    .then(res => res.json())
    .then(data => {
-    if (data.mensajes.length > 0) {
-     Swal.fire({
-      title: "Error en el formulario",
-      text: "Por favor, verifica los campos marcados como incorrectos.",
-      icon: "error",
-      confirmButtonText: "Aceptar",
-      customClass: {
-       popup: "custom-swal-popup",
-       title: "custom-swal-title",
-       content: "custom-swal-content"
-      }
-     });
-     return;
-    }
-    if (data.mensajes.length > 0) {
-     Swal.fire({
-      title: "Registro exitoso",
-      text: "El registro se ha realizado con éxito.",
-      icon: "success",
-      confirmButtonText: "Aceptar",
-      customClass: {
-       confirmButton: "custom-confirm-button"
-      }
-     }).then(result => {
-      if (result.isConfirmed) {
-       setOpen(false);
-      }
-     });
-     return;
-    }
+console.log(data);
    });
  };
 
